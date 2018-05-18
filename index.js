b4ffe3c3d7c12ac8866578d45c1d705c1614a786
Jörg Ossenkopp <ossenkopp@pollion.com>
2018-05-18 11:07:28 +0200

    Make json available for es6 import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const iso_639_1 = require('./data/iso_639-1');
-const iso_639_2 = require('./data/iso_639-2');
+import { iso_639_1 } from './data/iso_639-1';
+import { iso_639_2 } from './data/iso_639-2';
 
-module.exports = {
+export {
   iso_639_1,
   iso_639_2
 };
